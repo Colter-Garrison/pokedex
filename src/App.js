20c3cf67e2b6caff64b32c1c7cbb85cbddeb0462
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-06-28 17:22:56 -0700

    forgetting to ACP...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getPokemon } from './services/fetch-utils';
+import { getYelp } from './services/fetch-utils';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonQuery, setPokemonQuery] = useState([]);
+  const [businesses, setBusinesses] = useState([]);
+  const [yelpQuery, setYelpQuery] = useState([]);
+  
+  async function fetchAndStorePokemon() {
+    const data = await getPokemon(pokemonQuery);
+    setPokemon(data.results);
+  }
+
+  async function fetchAndStoreYelp() {
+    const data = await getYelp(yelpQuery);
+    setBusinesses(data.businesses);
+  }
+
+  useEffect(() => {
+    fetchAndStorePokemon();
+    fetchAndStoreYelp();
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await fetchAndStorePokemon();
+    setPokemonQuery('');
+  }
+  
+  async function handleYelpSubmit(e) {
+    e.preventDefault();
+    await fetchAndStoreYelp();
+    setYelpQuery('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='pokemon-list'>
+        <form onSubmit={handleSubmit}>
+          <input onChange={e => setPokemonQuery(e.target.value)} />
+          <button>Search</button>
+        </form>
+        {
+          pokemon.map((poke, i) => <div
+            key={poke.pokemon + i} className='pokemon'>
+            <img src={poke.url_image} />
+            <p>{poke.pokemon}</p>
+          </div>)
+        }
+      </div>
+      <div className='yelp-list'>
+        <form onSubmit={handleYelpSubmit}>
+          <input onChange={e => setYelpQuery(e.target.value)} />
+          <button>Search</button>
+        </form>
+        {
+          businesses.map((business, i) => <div
+            key={business.name + i} className='business'>
+            <p>{business.name}</p>
+          </div>)
+        }
+      </div>
     </div>
   );
 }
